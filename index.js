9b874fd1bcf8f3089da080eaf27c7951357a3dd5
Mohan N <Mohan14532@gmail.com>
2026-07-23 23:28:10 +0530

    Rename UserRoutes to userRoutes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from  "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
 import authRoutes from "./Routes/authRoutes.js";
 import videoRoutes from "./Routes/videoRoutes.js";
 import commentRoutes from './Routes/commentRoutes.js';
@@ -16,19 +15,24 @@ import path from "path";
 import { fileURLToPath } from "url";
 import historyRoutes from "./Routes/historyRoutes.js";
 import RazorpayRoutes from "./Routes/RazorpayRoutes.js";
-import userRoutes from "./Routes/userRoutes.js";
+import UserRoutes from "./Routes/UserRoutes.js";
 import downloadRoutes from "./Routes/downloadRoutes.js";
 
 dotenv.config()
 const app=express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.get("/",(req,res)=> {
     res.send("stream link backend is working")
 })
-app.use(bodyParser.json())
+
 const PORT=process.env.PORT||5000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
