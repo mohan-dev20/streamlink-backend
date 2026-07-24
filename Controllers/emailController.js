95c2d58b80bcae5f43dda39d8dd451a14cfb7f2d
Mohan N <Mohan14532@gmail.com>
2026-07-25 00:34:59 +0530

    email export

--- Controllers/emailController.js
+++ Controllers/emailController.js
@@ -1,129 +1,36 @@
-import nodemailer from "nodemailer";
+import  resend from "../Utils/resend.js";
+
 export const sendInvoiceEmail = async (req, res) => {
   try {
     const { email, plan, amount } = req.body;
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-    const html = `
-<div style="font-family:Arial;background:#f5f7fb;padding:30px;">
 
-<div style="
-max-width:700px;
-margin:auto;
-background:white;
-padding:35px;
-border-radius:18px;
-box-shadow:0 8px 25px rgba(0,0,0,.1);
-">
-
-<h1 style="
-text-align:center;
-color:#2563eb;
-font-size:40px;
-">
-▶ StreamLink
-</h1>
-
-<h2 style="
-text-align:center;
-color:#16a34a;
-">
-🎉 Payment Successful
-</h2>
-
-<p style="
-text-align:center;
-font-size:18px;
-color:#555;
-">
-Thank you for upgrading your StreamLink Premium Plan.
-</p>
-
-<hr>
-
-<table style="
-width:100%;
-font-size:18px;
-">
-
-<tr>
-<td><b>Plan</b></td>
-<td>${plan}</td>
-</tr>
-
-<tr>
-<td><b>Amount Paid</b></td>
-<td>₹${amount}</td>
-</tr>
-
-<tr>
-<td><b>Date</b></td>
-<td>${new Date().toLocaleString()}</td>
-</tr>
-
-<tr>
-<td><b>Status</b></td>
-<td style="color:green;">
-SUCCESS
-</td>
-</tr>
-
-</table>
-
-<hr>
-
-<h3>Premium Benefits</h3>
-
-<ul>
-
-<li>✔ Unlimited Streaming</li>
-
-<li>✔ HD Video Quality</li>
-
-<li>✔ Premium Downloads</li>
-
-<li>✔ Faster Playback</li>
-
-<li>✔ Premium Features Enabled</li>
-
-</ul>
-
-<div style="
-margin-top:35px;
-text-align:center;
-color:#777;
-">
-
-Thank you for choosing StreamLink ❤️
-
-<br><br>
-
-Need Help?
-
-<br>
-
-support@streamlink.com
-
-</div>
-
-</div>
-
-</div>
-`;
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    await resend.emails.send({
+      from: "StreamLink <onboarding@resend.dev>",
       to: email,
-      subject: `🎉 ${plan} Plan Activated`,
-      html,
+      subject: `${plan} Plan Activated`,
+      html: `
+        <h2>Payment Successful</h2>
+
+        <p>Plan: ${plan}</p>
+
+        <p>Amount: ₹${amount}</p>
+
+        <p>Thank you for upgrading to StreamLink Premium.</p>
+      `,
     });
-    res.json({ success: true });
-  } catch (error) {
-    console.log("EMAIL ERROR:", error);
-    res.status(500).json({ message: error.message });
+
+    res.json({
+      success: true,
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+
   }
 };
