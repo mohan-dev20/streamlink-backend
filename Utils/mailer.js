9ff38ef3b7176a3834bcb6dd6348bfec825a8cfd
Mohan N <Mohan14532@gmail.com>
2026-07-25 09:00:56 +0530

    Switch OTP and invoice email to Brevo SMTP

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -1,21 +1,13 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4, // Force IPv4
-});
-transporter.verify((err, success) => {
-  if (err) {
-    console.log("SMTP ERROR:", err);
-  } else {
-    console.log("SMTP READY");
-  }
 });
 
 export default transporter;
