6f25e8fd36a61e53a777065af8f344056ec957fb
Mohan N <Mohan14532@gmail.com>
2026-07-25 23:17:56 +0530

    Changed to UploadCloudniary

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,67 +1,65 @@
-import User from "../Modals/Auth.js";
-import  {uploadToCloudinary} from "../Utils/cloudinary.js";
+import { v2 as cloudinary } from "cloudinary";
+import streamifier from "streamifier";
 
-export const updateProfile = async (req, res) => {
-  try {
-    const { username, bio, city } = req.body;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-    const updateData = {
-      username,
-      bio,
-      city,
-    };
 
-   if (req.file) {
-  const uploadedVideo = await cloudinary.uploader.upload(
-  req.files.video[0].path,
-  {
-    resource_type: "video",
-    folder: "streamlink/videos",
-  }
-);
-const uploadedThumbnail = await cloudinary.uploader.upload(
-  req.files.thumbnail[0].path,
-  {
-    folder: "streamlink/thumbnails",
-  }
-);
+// Normal upload (profile images, thumbnails)
+export const uploadToCloudinary = (
+  buffer,
+  folder,
+  resourceType = "auto"
+) => {
+  return new Promise((resolve, reject) => {
 
-  updateData.profilePic = uploaded.secure_url;
-}
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder,
+        resource_type: resourceType,
+      },
 
-    const user = await User.findByIdAndUpdate(
-      req.params.id,
-      updateData,
-      { new: true }
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
     );
 
-    res.json({
-      success: true,
-      user,
-    });
-
-  } catch (err) {
-    console.log(err);
-
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
 };
-export const getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id).select("-password");
 
-    res.json({
-      success: true,
-      user,
-    });
 
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
+// Large video upload (500MB support)
+export const uploadLargeToCloudinary = (
+  filePath,
+  folder,
+  resourceType = "video"
+) => {
+  return new Promise((resolve, reject) => {
+
+    cloudinary.uploader.upload_large(
+      filePath,
+      {
+        folder,
+        resource_type: resourceType,
+        chunk_size: 6000000,
+      },
+
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+
+  });
 };
